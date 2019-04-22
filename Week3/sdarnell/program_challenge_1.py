6963a3f936d4948fae9e4397015b27626669124d
Darnell <stephen.darnell@intel.com>
2019-04-22 09:27:15 -0700

    program which detects different file types

---
+++ Week3/sdarnell/program_challenge_1.py
@@ -0,0 +1,21 @@
+#This program will reading a set of files and printout their file types
+from os import listdir
+
+import os
+path = "C:\\Users\\sdarnell\\python101\\Python-101-2019\\Week3\\files"
+files = os.listdir(path)
+for file in files:
+    if "doc" in file:
+        print(file + " is a document")
+    elif "java" in file:
+        print(file + " is a java file")
+    elif "py" in file:
+        print(file + " is a python file")
+    elif "xlsx" in file:
+        print(file + " is an Execl file")
+    else:
+        print("unknown file type")
+
+
+
+
